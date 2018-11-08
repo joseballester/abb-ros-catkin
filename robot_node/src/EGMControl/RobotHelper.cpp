9dd5384c6739a7e66c972c76442fc9f62cb153c7
Jose Ballester <jose@ballester.me>
2018-11-08 09:32:51 -0500

    Added EGM control dependencies

--- robot_node/src/EGMControl/RobotHelper.cpp
+++ robot_node/src/EGMControl/RobotHelper.cpp
@@ -6,8 +6,9 @@ RobotHelper::RobotHelper(ros::NodeHandle n, int udpPort, limits x_limits, limits
   : seqno(0), start_tick(get_tick()), udpPort(udpPort), x_limits(x_limits), y_limits(y_limits), z_limits(z_limits)
 {
   sock = new UDPSocket(udpPort);
+  sock->setBlocking();
   flush_robot_data();
-  sock->setTimeout(500000);
+  sock->setTimeout(0, 500000);
   last_sent_ps = last_measured_ps;
 }
 
@@ -18,12 +19,12 @@ RobotHelper::~RobotHelper()
 
 void RobotHelper::flush_robot_data()
 {
-  messageSize = sock->recvFrom(inBuffer, MAX_BUFFER-1, sourceAddr, sourcePort);
+  messageSize = sock->recvFrom(inBuffer, MAX_ROBOT_BUFFER-1, sourceAddr, sourcePort);
   last_egm_robot = new abb::egm::EgmRobot();
 
   if (messageSize < 0) {
     // TODO: Should be improved
-    ROS_INFO("[EGMControl] Failed to receive message from robot");
+    ROS_INFO("ROBOT_CONTROLLER: Failed to receive EGM message from robot.");
     return;
   }
   last_egm_robot->ParseFromArray(inBuffer, messageSize);
@@ -51,7 +52,7 @@ geometry_msgs::PoseStamped RobotHelper::send_command(geometry_msgs::PoseStamped 
     } else {
       target = command_pose.pose;
     }
-    last_egm_sensor = Velocity_to_EgmSensor(target, seqno++, get_tick()-start_tick);
+    last_egm_sensor = Velocity_to_EgmSensor(target, last_measured_ps.pose, seqno++, get_tick()-start_tick);
   } else {
     if (command_pose.header.stamp == ros::Time(0)) {
       target = last_sent_ps.pose;
